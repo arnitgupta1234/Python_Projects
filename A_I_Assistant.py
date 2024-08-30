f9ba32e7f1d979dcb8a0372e065688a657618b1b
ARNIT GUPTA <76506336+arnitgupta1234@users.noreply.github.com>
2024-08-30 15:01:21 +0530

    Add files via upload

---
+++ A_I_Assistant.py
@@ -0,0 +1,158 @@
+import speech_recognition as sr
+import pyttsx3
+import wikipedia
+import datetime
+import webbrowser
+import os
+import time
+import subprocess
+from ecapture import ecapture as ec
+import wolframalpha
+import json
+import requests
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def wishMe():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif hour>=12 and hour<17:
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
+    else:
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio=r.listen(source)
+
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
+
+print("Loading your personal assistant X")
+speak("Loading your personal assistant X")
+wishMe()
+
+if __name__=='__main__':
+
+
+    while True:
+        speak("Tell me how can I help you now?")
+        statement = takeCommand().lower()
+        if statement==0:
+            continue
+
+        if "goodbye" in statement or "ok bye" in statement or "stop" in statement or "sayonara" in statement or "alvida" in statement or "chalo theek hai" in statement or "theek hai" in statement:
+            speak('your personal assistant X is shutting down,googbye')
+            print('your personal assistant X is shutting down,goodbye')
+            break
+
+        if 'wikipedia' in statement:
+            speak('Searching Wikipedia...')
+            print('Searching Wikipedia...\n')
+            statement =statement.replace("wikipedia", "")
+            results = wikipedia.summary(statement, sentences=5)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in statement:
+            webbrowser.open_new_tab("https://www.youtube.com")
+            speak("youtube is open now")
+            time.sleep(5)
+
+        elif 'open google' in statement:
+            webbrowser.open_new_tab("https://www.google.com")
+            speak("Google chrome is open now")
+            time.sleep(5)
+
+        elif 'open gmail' in statement:
+            webbrowser.open_new_tab("gmail.com")
+            speak("Google Mail open now")
+            time.sleep(5)
+
+        elif 'time' in statement:
+            strTime=datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"the time is {strTime}")
+
+        elif 'news' in statement:
+            news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/home/headlines")
+            speak('Here are some headlines from the Times of India,Happy reading')
+            time.sleep(6)
+
+        elif "camera" in statement or "take a photo" in statement:
+            ec.capture(0,"robo camera","img.jpg")
+
+        elif 'search'  in statement:
+            statement = statement.replace("search", "")
+            webbrowser.open_new_tab(statement)
+            time.sleep(5)
+
+        elif 'ask' in statement:
+            speak('I can answer to computational and geographical questions  and what question do you want to ask now')
+            question=takeCommand()
+            app_id="Paste your unique ID here "
+            client = wolframalpha.Client('V38T5E-KXXXXXXXXX')
+            res = client.query(question)
+            answer = next(res.results).text
+            speak(answer)
+            print(answer)
+
+        elif 'who are you' in statement or 'what can you do' in statement or 'tell me about yourself' in statement:
+            speak('I am X version 1 point O your personal assistant. I am programmed to minor tasks like'
+                  'opening youtube,google chrome, gmail and stackoverflow ,predict time,take a photo,search wikipedia,predict weather' 
+                  'In different cities, get top headline news from times of india and you can ask me computational or geographical questions too!')
+
+        elif 'who made you' in statement or 'who invented you' in statement:
+            speak('I am a personal assistent who is made by my father Arnit Gupta')
+
+        elif "weather" in statement:
+            api_key="cc69986d1b9fc1XXXXXXXXXXXX" #paste your Unique ID here
+            base_url="https://api.openweathermap.org/data/2.5/weather?"
+            speak("what is the city name")
+            city_name=takeCommand()
+            complete_url= base_url + "q=" + city_name + "&appid=" + api_key
+            response = requests.get(complete_url)
+            if response.status_code == 200:
+                x = response.json()
+                y=x['main']
+                current_temperature = y["temp"]
+                current_humidiy = y["humidity"]
+                z = x["weather"]
+                weather_description = z[0]["description"]
+                speak(" Temperature in kelvin unit is " +
+                      str(current_temperature) +
+                      "\n humidity in percentage is " +
+                      str(current_humidiy) +
+                      "\n description  " +
+                      str(weather_description))
+                print(" Temperature in kelvin unit = " +
+                      str(current_temperature) +
+                      "\n humidity (in percentage) = " +
+                      str(current_humidiy) +
+                      "\n description = " +
+                      str(weather_description))
+
+        elif "log off" in statement or "sign out" in statement:
+            speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+            subprocess.call(["shutdown", "/l"])
+
+    time.sleep(3)
+
